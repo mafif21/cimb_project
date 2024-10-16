a9c63e77028f39fe322ceb1c51c2cf7bbb60ff77
Muhammad Nurul Afif Maliki <afif.maliki21@gmail.com>
2024-10-17 00:35:41 +0700

    [MODIFY] update view for homepage desktop and handphone

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,51 +1,87 @@
 <x-app-layout>
     <x-slot name="title">Home</x-slot>
-
-    <x-slot name="hero">
-        <div class="leading-10 text-white mb-6 lg:w-1/3 ">
-            <h1 class="block text-6xl font-bold text-white mt-6 mb-4">Feel the taste of <span
-                    class="text-yellow-300 italic">Lamongan</span> food</h1>
-            <p class="lg:w-96 text-lg">Food tasted better when you eat it with your family and friends</p>
-        </div>
-
-        <div class="grid grid-cols-2 lg:w-1/4 gap-x-2.5 mb-6">
-            <a href="/food"
-                class="bg-red-600 rounded-lg px-6 py-3 shadow-md font-sans text-sm font-semibold text-white hover:opacity-50 text-center">View
-                Menu</a>
-        </div>
+    <x-slot name="additional">
+        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+            integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
     </x-slot>
 
     <x-slot name="slot">
-        <div class="superiority my-6 ">
-            <div class="grid lg:grid-cols-2 mb-5 lg:mb-0 items-center justify-items-center">
-                <div class="p-4 w-full">
-                    <img src="" class="rounded-lg object-cover h-80 w-full" alt="">
+        <div class="relative h-screen rounded-t-lg overflow-hidden md:grid md:grid-cols-[1fr_2fr]">
+            <div class="absolute bottom-0 h-[50vh] w-full md:relative">
+                <a href="{{ route('admin.index') }}"
+                    class="text-white hidden md:inline bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+                    Admin
+                </a>
+                <div class="p-4 md:p-6">
+                    <form class="mx-auto">
+                        <label for="default-search"
+                            class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Search</label>
+                        <div class="relative">
+                            <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                                <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true"
+                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                        stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
+                                </svg>
+                            </div>
+                            <input type="search" id="default-search"
+                                class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                placeholder="Search Mockups, Logos..." required />
+                            <button type="submit"
+                                class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                        </div>
+                    </form>
+
                 </div>
-                <div class="desc px-4 lg:px-6 h-fit">
-                    <h1 class="mb-3 font-bold text-3xl headings">High Quality Food Material</h1>
-                    @auth
-                        <a href="{{ route('admin.index') }}">Ini ke admin</a>
-                    @endauth
+                <div
+                    class="flex flex-nowrap justify-center px-4 overflow-y-auto gap-x-2 md:w-full md:px-6 md:flex-col gap-4">
+                    <a href="#"
+                        class="block w-full lg:w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy
+                            technology acquisitions 2021</h5>
+                        <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
+                            technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    </a>
+                    <a href="#"
+                        class="block w-full lg:w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy
+                            technology acquisitions 2021</h5>
+                        <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
+                            technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    </a>
+                    <a href="#"
+                        class="block w-full lg:w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100">
+
+                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy
+                            technology acquisitions 2021</h5>
+                        <p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise
+                            technology acquisitions of 2021 so far, in reverse chronological order.</p>
+                    </a>
                 </div>
             </div>
 
-            <div class="grid lg:grid-cols-2 items-center justify-items-center flex flex-col">
-                <div class="p-4  w-full">
-                    <img src="" class="rounded-lg object-cover h-80 w-full" alt="">
-                </div>
-                <div class="desc px-4 lg:px-6 h-fit order-last lg:order-first">
-                    <h1 class="mb-3 font-bold text-3xl headings">High Quality Food Material</h1>
-                    <p class="text-gray-400 pr-12">Bahan makanan yang kurang segar dapat menyebabkan masalah kesehatan
-                        pada pelanggan Kami. Oleh karena itu, Kami memastikan semua bahan makanan yang Kami gunakan
-                        segar, yang nantinya untuk membuat makanan yang enak dan sehat bagi pelanggan Kami.</p>
-
-                </div>
-            </div>
+            <div id="map" class="h-[50vh] md:h-screen"></div>
         </div>
+    </x-slot>
 
-        <div class="fav my-12">
-            <h1 class="font-black text-3xl text-center">Categories Of Food</h1>
+    <x-slot name="scripts">
+        <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+            integrity="sha512-djRf8Q5f5s/TNz/tLD9gZp3p2hkHvf0Sb1CO8t3hKmY8/diIcdUnP3cwPEqU7APLiRYf2zp8HM7mNpUYYa0XrA=="
+            crossorigin=""></script>
+        <script>
+            var map = L.map('map').setView([-6.914744, 107.609810], 13);
 
-        </div>
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker([-6.914744, 107.609810]).addTo(map)
+                .bindPopup('Hello from Bandung!')
+                .openPopup();
+        </script>
     </x-slot>
 </x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ $title ?? config('app.name') }}</title>
+    {{ $additional ?? '' }}
+
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@200;400;600;800&display=swap">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
@@ -14,14 +16,14 @@
 <body class="font-sans antialiased flex flex-col">
     <div class="min-h-screen bg-white">
         <main>
-            <x-container>
+            <div>
                 {{ $slot }}
-            </x-container>
+            </div>
         </main>
     </div>
-    <script src="https://unpkg.com/flowbite@1.5.5/dist/flowbite.js"></script>
 
-    </script>
+    {{ $scripts ?? '' }}
+    <script src="https://unpkg.com/flowbite@1.5.5/dist/flowbite.js"></script>
 </body>
 
 </html>
