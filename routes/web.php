b5a115d654a277673d4d065aa4f1f72fe6ca1b6a
Hendri Heryanto <hendri.heryanto8@gmail.com>
2024-10-17 12:51:53 +0700

    temp push landing page

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
+use App\Http\Controllers\LandingController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\BranchController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('dashboard.index');
-});
+
+Route::get('/', [LandingController::class, 'index'])->name('index');
 
 Route::middleware('auth')->name('admin.')->prefix('admin')->group(function () {
     Route::get('/', [AdminController::class, 'index'])->name('index');
