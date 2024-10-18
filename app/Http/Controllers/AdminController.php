541e9737bbcc2c3caa3352cd62e65f437472c7a9
Farrel Andhika <farrel.arp@gmail.com>
2024-10-18 09:13:53 +0700

    feat: add dashboard monitor

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -18,6 +18,16 @@ class AdminController extends Controller
             'totalBranches' => Branch::count(),
             'totalATM' => Branch::where('category_id', 1)->count(),
             'totalCDM' => Branch::where('category_id', 2)->count(),
+            'totalTST' => Branch::where('category_id', 3)->count(),
+            'totalKC' => Branch::where('category_id', 4)->count(),
+            'totalDigitalLounge' => Branch::where('category_id', 5)->count(),
+            'totalSubBranch' => Branch::where('category_id', 6)->count(),
+            'totalKiosk' => Branch::where('category_id', 7)->count(),
+            'totalKCS' => Branch::where('category_id', 8)->count(),
+            'totalKFS' => Branch::where('category_id', 9)->count(),
+            'totalKCPS' => Branch::where('category_id', 10)->count(),
+            'totalWeekendBanking' => Branch::where('category_id', 11)->count(),
+
         ];
         return view('admin.index', compact('data'));
     }
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -14,42 +14,64 @@
 
         <!-- Total Orders Card -->
         <div class="p-4 bg-white rounded-lg shadow-md">
-            <h3 class="text-sm font-medium text-gray-500">Total ATM</h3>
+            <h3 class="text-sm font-medium text-gray-500">Total ATM (Tarik Tunai)</h3>
             <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalATM'] }}</p>
         </div>
 
         <!-- Total Revenue Card -->
         <div class="p-4 bg-white rounded-lg shadow-md">
-            <h3 class="text-sm font-medium text-gray-500">Total CDM</h3>
+            <h3 class="text-sm font-medium text-gray-500">Total CDM (Setor Tunai)</h3>
             <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalCDM'] }}</p>
         </div>
 
         <!-- Products Sold Card -->
         <div class="p-4 bg-white rounded-lg shadow-md">
-            <h3 class="text-sm font-medium text-gray-500">Products Sold</h3>
-            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['productsSold'] }}</p>
+            <h3 class="text-sm font-medium text-gray-500">Total TST (Tarik Setor Tunai)</h3>
+            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalTST'] }}</p>
         </div>
         <div class="p-4 bg-white rounded-lg shadow-md">
-            <h3 class="text-sm font-medium text-gray-500">Total Users</h3>
-            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalUsers'] }}</p>
+            <h3 class="text-sm font-medium text-gray-500">Total Kantor Cabang</h3>
+            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalKC'] }}</p>
         </div>
 
         <!-- Total Orders Card -->
         <div class="p-4 bg-white rounded-lg shadow-md">
-            <h3 class="text-sm font-medium text-gray-500">Total Orders</h3>
-            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalOrders'] }}</p>
+            <h3 class="text-sm font-medium text-gray-500">Total Digital Lounge</h3>
+            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalDigitalLounge'] }}</p>
         </div>
 
         <!-- Total Revenue Card -->
         <div class="p-4 bg-white rounded-lg shadow-md">
-            <h3 class="text-sm font-medium text-gray-500">Total Revenue</h3>
-            <p class="mt-1 text-3xl font-semibold text-gray-900">${{ number_format($data['revenue'], 2) }}</p>
+            <h3 class="text-sm font-medium text-gray-500">Total KCP</h3>
+            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalSubBranch'] }}</p>
         </div>
 
         <!-- Products Sold Card -->
         <div class="p-4 bg-white rounded-lg shadow-md">
-            <h3 class="text-sm font-medium text-gray-500">Products Sold</h3>
-            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['productsSold'] }}</p>
+            <h3 class="text-sm font-medium text-gray-500">Total Kiosk</h3>
+            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalKiosk'] }}</p>
+        </div>
+        <div class="p-4 bg-white rounded-lg shadow-md">
+            <h3 class="text-sm font-medium text-gray-500">Total Kantor Cabang Syariah</h3>
+            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalKCS'] }}</p>
+        </div>
+
+        <!-- Total Orders Card -->
+        <div class="p-4 bg-white rounded-lg shadow-md">
+            <h3 class="text-sm font-medium text-gray-500">Total Kantor Fungsional Syariah</h3>
+            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalKFS'] }}</p>
+        </div>
+
+        <!-- Total Revenue Card -->
+        <div class="p-4 bg-white rounded-lg shadow-md">
+            <h3 class="text-sm font-medium text-gray-500">Total KCP Syariah</h3>
+            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalKCPS'] }}</p>
+        </div>
+
+        <!-- Products Sold Card -->
+        <div class="p-4 bg-white rounded-lg shadow-md">
+            <h3 class="text-sm font-medium text-gray-500">Total Weekend Banking</h3>
+            <p class="mt-1 text-3xl font-semibold text-gray-900">{{ $data['totalWeekendBanking'] }}</p>
         </div>
         
     </div>
